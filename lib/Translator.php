67641cd3b4544aa42828bb28d9a21f1c85a9ec16
michalpleban <michalpleban@gmail.com>
2019-04-14 22:49:54 +0200

    Action structure

--- lib/Translator.php
+++ lib/Translator.php
@@ -11,35 +11,134 @@ class Translator
     protected $line;
     
     protected $beforeSemicolon;
+    
+    protected $curlyLevel;
+    protected $bracketLevel;
+    protected $holdLevel;
+    
+    protected $curlyActions;
+    protected $bracketActions;
+    protected $holdActions;
 
     public function __construct($source)
     {
         $this->tokens = token_get_all($source);
     }
-    
+
     public function getResult()
     {
         $this->result = "";
         $this->beforeSemicolon = "";
 
+        $this->curlyLevel = $this->bracketLevel = $this->holdLevel = 0;
+        $this->curlyActions = $this->bracketActions = $this->holdActions = array();
+        
+        $this->holdAction();
+
         foreach($this->tokens as $token)
         {
             if(is_array($token))
             {
-                $this->handleToken($token[2], token_name($token[0]), $token[1]);
+                if($token[0] == T_WHITESPACE)
+                {
+                    $this->emit($token[1]);
+                }
+                else
+                {
+                    $this->handleToken($token[2], token_name($token[0]), $token[1]);
+                }
             }
             else
             {
-                $this->handleCharacter($token);
+                switch($token)
+                {
+                    case "{":
+                        $this->curlyOpen();
+                        break;
+                    case "}":
+                        $this->curlyClose();
+                        break;
+                    case "(":
+                        $this->bracketOpen();
+                        break;
+                    case ")":
+                        $this->bracketClose();
+                        break;
+                    default:
+                        $this->handleCharacter($token);
+                }
             }
         }
+        
+        $this->holdFinalize();
         
         return $this->result;
     }
     
-    protected function emit($code)
+    protected function emit($text)
     {
-        $this->result .= $code;
+        if($this->holdLevel > 0)
+        {
+            $function = $this->holdActions[$this->holdLevel-1]["fnEmit"];
+            $this->$function($text);
+        }
+        else
+        {
+            $this->result .= $text;
+        }
+    }
+    
+    protected function holdAction($fnEmit = "holdCopy", $content = array())
+    {
+        $this->holdActions[$this->holdLevel] = $content;
+        $this->holdActions[$this->holdLevel]["fnEmit"] = $fnEmit;
+        $this->holdActions[$this->holdLevel]["content"] = "";
+        $this->holdLevel++;
+    }
+    
+    protected function holdCopy($text)
+    {
+        $this->holdActions[$this->holdLevel-1]["content"] .= $text;
+    }
+    
+    protected function holdFinalize()
+    {
+        $this->emit($this->holdActions[--$this->holdLevel]["content"]);
+    }
+    
+    protected function bracketAction($fnFinish, $content = array())
+    {
+        $this->bracketActions[$this->bracketLevel] = $content;
+        $this->bracketActions[$this->bracketLevel]["fnFinish"] = $fnFinish;
+    }
+    
+    protected function curlyOpen()
+    {
+        $this->curlyLevel++;
+        $this->emit("{");
+    }
+    
+    protected function curlyClose()
+    {
+        $this->emit("}");
+        $this->curlyLevel--;
+    }
+    
+    protected function bracketOpen()
+    {
+        $this->bracketLevel++;
+        $this->emit("(");
+    }
+    
+    protected function bracketClose()
+    {
+        $this->emit(")");
+        $this->bracketLevel--;
+        if($this->bracketActions[$this->bracketLevel])
+        {
+            $function = $this->bracketActions[$this->bracketLevel]["fnFinish"];
+            $this->$function($this->bracketActions[$this->bracketLevel]);
+        }
     }
     
     protected function handleCharacter($char)
@@ -88,11 +187,6 @@ class Translator
     protected function T_COMMENT($text)
     {
         // Do nothing
-    }
-
-    protected function T_WHITESPACE($text)
-    {
-        $this->emit($text);
     }
 
     protected function T_LNUMBER($text)
@@ -158,7 +252,7 @@ class Translator
 
     protected function T_PRINT($text)
     {
-        return $this->T_ECHO($text);
+        $this->T_ECHO($text);
     }
 }
 
